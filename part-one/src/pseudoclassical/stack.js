855bd6ef877d623afa79a7d1dfcb0ada0cbfda52
Younghee Rhee <rheey90@gmail.com>
2019-07-25 19:25:46 +0900

    Completed pseudoclassical stack

--- part-one/src/pseudoclassical/stack.js
+++ part-one/src/pseudoclassical/stack.js
@@ -1,7 +1,29 @@
 const Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.seq = 0;
+  this.count = 0;
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.seq] = value;
+  this.seq++;
+  this.count++;
+}
+
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    this.seq--;
+    var output = this.storage[this.seq];
+    delete this.storage[this.seq];
+    this.count--;
+    return output;
+  }
+}
+
+Stack.prototype.size = function() {
+  return this.count;
+}
+
 
 module.exports = {
   Stack
