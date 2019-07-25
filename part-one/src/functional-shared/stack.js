2515fa201e1ffd63286cde60e5bb5f371e4bcd87
Younghee Rhee <rheey90@gmail.com>
2019-07-25 17:12:58 +0900

    In progress

--- part-one/src/functional-shared/stack.js
+++ part-one/src/functional-shared/stack.js
@@ -1,9 +1,42 @@
 const Stack = function () {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    seq: 0,
+    count: 0,
+    storage: {}
+  };
+  extend(someInstance, stackMethods);
+  return someInstance;
+};
+
+var extend = function (to, from) {
+  for (let key in from) {
+    to[key] = from[key]
+  }
 };
 
 const stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.seq] = value;
+  this.seq++;
+  this.count++;
+}
+
+stackMethods.pop = function() {
+  if (this.count > 0) {
+    this.seq--;
+    var output = this.storage[this.seq];
+    delete this.storage[this.seq];
+    this.count--;
+    return output;
+  }
+}
+
+stackMethods.size = function() {
+  return this.count;
+}
+
+
 
 if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
